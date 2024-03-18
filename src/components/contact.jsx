628fae0933f364e8838a68181db45247f292c7ea
TPride <tranielmp8@gmail.com>
2024-03-17 19:54:37 -0500

    form submission

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -17,7 +17,7 @@ export default function Contact() {
           <input name="name" className='w-full lg:my-3 my-6 rounded-lg bg-gray-500 p-4 border-2 border-red-800  text-xl ' placeholder='Enter Your Name' type="text" />
           <input className='w-full lg:my-3 my-6 rounded-lg bg-gray-500 p-4 border-2 border-red-800  text-xl ' placeholder='Enter Your Email' type="email" />
         </div>
-        <textarea name="message" className='w-full my-4 rounded-lg bg-gray-500 p-4 border-2 border-red-800  text-xl text-slate-100'  placeholder='Enter Your Message' name="" id="" cols="20" rows="10"></textarea>
+        <textarea name="message" className='w-full my-4 rounded-lg bg-gray-500 p-4 border-2 border-red-800  text-xl text-slate-100'  placeholder='Enter Your Message' id="" cols="20" rows="10"></textarea>
         <button className='neno-button shadow-xl hover:shadow-red-800/50 border-2 border-red-800  rounded-lg py-4 px-8 my-6 uppercase relative overflow-hidden tech-glow text-2xl text-bold text-white hover:text-black hover:bg-red-600'  type='submit'>Submit</button>
       </form>
       </div>
